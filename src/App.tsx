6c52d4cce9512648285317cc1cc1f9769d5d3748
Apurv-07 <mailapurva007@gmail.com>
2026-08-26 00:33:10 +0530

    Created todo page with all functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todoItem, setTodoItem] = useState("")
+    const [todos, setTodos] = useState<any>([])
+    const [currentItem, setCurrentItem]=useState<{_id?:string; todo?:string; status?: boolean;}>({})
+    useEffect(() => {
+        const getData = async () => {
+            const todoResponse = await fetch('http://localhost:8000/todo/todos', {
+                credentials: 'include',
+                method: 'GET'
+            })
+            const data = await todoResponse.json();
+            console.log(data)
+            setTodos(data.todos)
+        }
+        getData();
+    }, [])
+    const handleAdd = async () => {
+        const addTodo = await fetch("http://localhost:8000/todo/todos", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                todo: todoItem,
+            }),
+        });
+        const result = await addTodo.json();
+        if (addTodo.status === 200) {
+            alert('Todo added successfully')
+            console.log(result)
+            setTodos([...todos, result.newTodo])
+        }
+    }
+    const handleEdit = async (id?: string, status: boolean = false, todo: string = "") => {
+        if (!id) return;
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
+        const res = await fetch(`http://localhost:8000/todo/todos/${id}`, {
+            method: 'PUT',
+            credentials: 'include',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                todo,
+                status
+            })
+        })
+        const data = await res.json();
+        if (res.status == 200) {
+            alert("Updated")
+            const updatedItem = todos.map((item: any) => item._id == id ? data.updatedTodo : item)
+            setTodos([...updatedItem])
+        }
+    }
+    const handleEditField = (item: any) => {
+        setCurrentItem(item)
+    }
+    const handleDelete = async(id: string)=>{
+        const res = await fetch(`http://localhost:8000/todo/todos/${id}`, {
+            method: "DELETE",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const data = await res.json();
+        if(res.status==200){
+            alert(data.message)
+            const newTodos = todos.filter((item:any)=>item._id!=id)
+            console.log("JJJJJ", newTodos)
+            setTodos([...newTodos])
+        }
+    }
+    return (
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+            <input type="text" value={todoItem} onChange={(e) => setTodoItem(e.target.value)} />
+            <button onClick={handleAdd}>Add</button>
+            {todos.map((item:any)=>(
+                <div key={item._id}>
+        <input
+          type="checkbox"
+          id="subscribe-checkbox"
+          checked={item.status}
+          onChange={(e)=>handleEdit(item._id, e.target.checked, item.todo)}
+        />
+                    {item._id!=currentItem._id?item.todo:<input value={currentItem.todo} onChange={(e)=>setCurrentItem({...currentItem, todo: e.target.value})} />}
+                    {currentItem._id!=item._id?<button onClick={()=>{handleEditField(item)}}>Edit</button>:
+                    <button onClick={()=>{handleEdit(currentItem._id, currentItem.status, currentItem.todo)}}>Update</button>}
+                    <button onClick={()=>{handleDelete(item._id)}}>Delete</button>
+                </div>
+            ))}
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    )
 }
 
 export default App
